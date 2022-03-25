fab2467d6733d5549a85a6ee921869cef3f1da76
VietHoang319 <arishoang319@gmail.com>
2022-03-25 20:04:50 +0700

    checkmate rock and queen

--- JS/Move/Rook.js
+++ JS/Move/Rook.js
@@ -52,14 +52,22 @@ function moveRook(team, a, b) {
 function checkmateRook(team, a, b) {
     let x = +a;
     let y = +b;
+    let countr = 0;
+    let countb = 0;
 
     // Thẳng dưới
     while (checkXY(x - 1, y) && getTeam(x - 1, y) !== team){
         if (getTeam(x - 1, y) !== team && getTeam(x - 1, y) !== ""){
             if (arrMap[x-1][y] === "rKing") {
+                countr += 1;
+                checkR = a.toString()+b.toString();
+                chessCheckR = arrMap[+a][+b];
                 checkmateR = true;
             }
             if (arrMap[x-1][y] === "bKing") {
+                countb += 1;
+                checkB = a.toString()+b.toString();
+                chessCheckB = arrMap[+a][+b];
                 checkmateB = true;
             }
             break;
@@ -73,9 +81,15 @@ function checkmateRook(team, a, b) {
     while (checkXY(x + 1, y) && getTeam(x + 1, y) !== team){
         if (getTeam(x + 1, y) !== team && getTeam(x + 1, y) !== ""){
             if (arrMap[x+1][y] === "rKing") {
+                countr += 1;
+                checkR = a.toString()+b.toString();
+                chessCheckR = arrMap[+a][+b];
                 checkmateR = true;
             }
             if (arrMap[x+1][y] === "bKing") {
+                countb += 1;
+                checkB = a.toString()+b.toString();
+                chessCheckB = arrMap[+a][+b];
                 checkmateB = true;
             }
             break;
@@ -89,9 +103,15 @@ function checkmateRook(team, a, b) {
     while (checkXY(x, y + 1) && getTeam(x, y + 1) !== team){
         if (getTeam(x, y + 1) !== team && getTeam(x, y + 1) !== ""){
             if (arrMap[x][y+1] === "rKing") {
+                countr += 1;
+                checkR = a.toString()+b.toString();
+                chessCheckR = arrMap[+a][+b];
                 checkmateR = true;
             }
             if (arrMap[x][y+1] === "bKing") {
+                countb += 1;
+                checkB = a.toString()+b.toString();
+                chessCheckB = arrMap[+a][+b];
                 checkmateB = true;
             }
             break;
@@ -105,13 +125,33 @@ function checkmateRook(team, a, b) {
     while (checkXY(x, y - 1) && getTeam(x, y - 1) !== team){
         if (getTeam(x, y - 1) !== team && getTeam(x, y - 1) !== ""){
             if (arrMap[x][y-1] === "rKing") {
+                countr += 1;
+                checkR = a.toString()+b.toString();
+                chessCheckR = arrMap[+a][+b];
                 checkmateR = true;
             }
             if (arrMap[x][y-1] === "bKing") {
+                countb += 1;
+                checkB = a.toString()+b.toString();
+                chessCheckB = arrMap[+a][+b];
                 checkmateB = true;
             }
             break;
         }
         y -= 1;
+    }
+    if (team === "b") {
+        if (countr === 0) {
+            checkR = "";
+            chessCheckR = "";
+            checkmateR = false;
+        }
+    }
+    if (team === "r") {
+        if (countb === 0) {
+            checkB = "";
+            chessCheckB = "";
+            checkmateB = false;
+        }
     }
 }
